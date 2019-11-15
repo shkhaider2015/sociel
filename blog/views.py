4e94acd8657a198541126ea46652920e7106876b
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-11-15 08:21:55 +0500

    pagination complete with user pagination

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 
 # posts = [
 #     {
@@ -34,7 +35,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
-    paginate_by = 4
+    paginate_by = 5
 
 
 class PostDetailView(DetailView):
@@ -73,4 +74,16 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         if self.request.user == post.author:
             return True
         return False
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
+
     
