cc731c7eaa1148aae66dff1a50a495b177d4986d
Shakeel Haider <43218437+shkhaider2015@users.noreply.github.com>
2019-11-18 08:45:23 +0500

    password reset complete

--- sociel/urls.py
+++ sociel/urls.py
@@ -29,6 +29,7 @@ urlpatterns = [
     path('password-reset/', auth_view.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
     path('password-reset/done', auth_view.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset-complete/', auth_view.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'),
     path('', include('blog.urls') )
 ]
 
